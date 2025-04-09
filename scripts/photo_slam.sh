8c308b66ebc315a7376436e9f4490765807cb2fe
kana_takizawa@brown.edu <ktakiza1@vscode1.oscar.ccv.brown.edu>
2025-04-08 21:49:18 -0400

    using BDW dataset

--- scripts/photo_slam.sh
+++ scripts/photo_slam.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# APMA Dataset
+# ../bin/euroc_stereo \
+#     ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
+#     ../cfg/ORB_SLAM3/Stereo/BPOD/BPOD.yaml \
+#     ../cfg/gaussian_mapper/Stereo/EuRoC/EuRoC.yaml \
+#     ../data/BPOD/apma_backward_auto_exposure \
+#     ../cfg/ORB_SLAM3/Stereo/BPOD/apma_timestamps.txt \
+#     ../results/bpod/apma_backward_auto_exposure \
+#     no_viewer
+
+# BDW Dataset
 ../bin/euroc_stereo \
     ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
     ../cfg/ORB_SLAM3/Stereo/BPOD/BPOD.yaml \
     ../cfg/gaussian_mapper/Stereo/EuRoC/EuRoC.yaml \
-    ../data/BPOD/apma_backward_auto_exposure \
-    ../cfg/ORB_SLAM3/Stereo/BPOD/timestamps.txt \
-    ../results/bpod/apma_backward_auto_exposure \
+    ../data/BPOD/BDW_backward_auto \
+    ../cfg/ORB_SLAM3/Stereo/BPOD/BDW_timestamps.txt \
+    ../results/bpod/BDW_backward_auto \
     no_viewer
